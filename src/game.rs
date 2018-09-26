60928a57cf29fec941bf1e0c494a8be3982b7326
Stipendi <turvatili@icloud.com>
2018-09-26 21:17:02 +0300

    Introducing the game struct

---
+++ src/game.rs
@@ -0,0 +1,18 @@
+use piston_window::{
+    Context,
+    G2d,
+};
+pub struct Game {
+
+}
+
+impl Game {
+    pub fn new() -> Game {
+        Game {
+
+        }
+    }
+    pub fn render(c: Context, g: &G2d) {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
 extern crate piston_window;
+mod game;
 use piston_window::{
-    glutin_window::GlutinWindow,
-    WindowSettings
+    PistonWindow,
+    WindowSettings,
+    clear
+};
+use game::{
+    Game
 };
 
 fn main() {
-    let mut window: GlutinWindow = WindowSettings::new("Tetris!", (200, 400))
+    let mut window: PistonWindow = WindowSettings::new("Tetris!", (200, 400))
         .build()
         .expect("Fatal: Failed to build window!");
+    let game = Game::new();
     while let Some(e) = window.next() {
         window.draw_2d(&e, |_c, g| {
+
             clear([0.5, 1.0, 0.5, 1.0], g);
         });
     }
