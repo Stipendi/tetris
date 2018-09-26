739ea3da1600ebddbc4faa05e9a7e7ff9a595fd8
Stipendi <turvatili@icloud.com>
2018-09-26 20:47:16 +0300

    It now creates a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+extern crate piston_window;
+use piston_window::{
+    glutin_window::GlutinWindow,
+    WindowSettings
+};
+
 fn main() {
-    println!("Hello, world!");
+    let mut window: GlutinWindow = WindowSettings::new("Tetris!", (200, 400))
+        .build()
+        .expect("Fatal: Failed to build window!");
+    while let Some(e) = window.next() {
+        window.draw_2d(&e, |_c, g| {
+            clear([0.5, 1.0, 0.5, 1.0], g);
+        });
+    }
 }
